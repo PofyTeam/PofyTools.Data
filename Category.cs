2335dc11b1e789a84c14a7093aca21e26bbcd75e
Danilo Čoda Vukomanović <dvukomanovic@gmail.com>
2018-07-31 07:42:46 +0200

    added category

---
+++ Category.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PofyTools.Data
+{
+
+    [System.Serializable]
+    public class CategoryDefinition : Definition
+    {
+        public CategoryDefinition(string key)
+        {
+            this.id = key;
+        }
+
+        [Header("Display Name")]
+        public string displayName;
+
+        [TextArea]
+        [Header("Category Description")]
+        public string categoryDescription;
+
+        [Header("Base Categories")]
+        public List<string> baseCategories = new List<string>();
+
+        //[Header("NameSet")]
+        //public NameSet nameSet;
+        //public NameSet influenceSet;
+    }
+
+    public class CategoryData : DefinableData<CategoryDefinition>
+    {
+        public CategoryData(CategoryDefinition definition) : base(definition) { }
+
+        #region API
+        public void AddSubcategory(CategoryData data)
+        {
+            this.subcategories.Add(data.id);
+        }
+        #endregion
+
+        #region Runtime Data
+        public List<string> subcategories = new List<string>();
+        #endregion
+    }
+
+    public class CategoryDataSet : DataSet<string, CategoryData>
+    {
+        public CategoryDataSet(DefinitionSet<CategoryDefinition> categoryDefinitionSet)
+        {
+            Initialize(categoryDefinitionSet.GetContent());
+        }
+
+        /// <summary>
+        /// Topmost categories.
+        /// </summary>
+        public List<CategoryData> rootCategories = new List<CategoryData>();
+
+        public bool Initialize(List<CategoryDefinition> categoryDefs)
+        {
+            if (!this.IsInitialized)
+            {
+                this.content = new Dictionary<string, CategoryData>(categoryDefs.Count);
+
+                foreach (var category in categoryDefs)
+                {
+                    CategoryData data = new CategoryData(category);
+
+                    //list
+                    this._content.Add(data);
+
+                    //dictionary
+                    this.content[data.id] = data;
+
+                    if (category.baseCategories.Count == 0)
+                    {
+                        this.rootCategories.Add(data);
+                    }
+                }
+
+                Initialize();
+
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Initialize()
+        {
+            if (!this.IsInitialized)
+            {
+
+                foreach (var data in this._content)
+                {
+                    foreach (var baseCategory in data.Definition.baseCategories)
+                    {
+                        CategoryData baseData;
+                        if (this.content.TryGetValue(baseCategory, out baseData))
+                        {
+                            baseData.AddSubcategory(data);
+                        }
+                    }
+                }
+                PofyTools.UI.NotificationView.Show("Game Definitions Initialized!", null, -1f);
+                this.IsInitialized = true;
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
